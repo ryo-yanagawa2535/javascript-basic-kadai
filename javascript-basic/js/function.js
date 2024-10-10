e1bf6c63ff621898d256a62fa218e012dea6ecfe
ryo-yanagawa2535 <ryosan1108@gmail.com>
2024-10-10 20:27:03 +0900

    提出

---
+++ javascript-basic/js/function.js
@@ -0,0 +1,18 @@
+ // 朝のあいさつを出力する関数を定義する
+ const sayGoodMorning = () => {
+  console.log('おはようございます！');
+  console.log('昨日はよく眠れましたか？');
+  console.log('今日も一日頑張りましょう！');
+}
+
+// 夜のあいさつを出力する関数を定義する
+const sayGoodEvening = () => {
+  console.log('こんばんは！');
+  console.log('今日も一日お疲れさまでした。');
+}
+
+// 朝のあいさつを出力する関数を呼び出す
+sayGoodMorning();
+
+// 夜のあいさつを出力する関数を呼び出す
+sayGoodEvening();
