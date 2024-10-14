22c5513ef9e7b38edbcee4c69cc267f481846fa9
ryo-yanagawa2535 <ryosan1108@gmail.com>
2024-10-14 11:51:01 +0900

    提出

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -2,7 +2,7 @@
 const holidays = ["正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"];
 
 // for文を使用して配列の要素を出力
-for(let i = 0; i < holidays.length; i++) {
+for(let i = 0; i < 16 ; i++) {
   console.log(holidays[i]);
 }
 
@@ -10,6 +10,6 @@ for(let i = 0; i < holidays.length; i++) {
 let i = 0;
 
 // while文を使用して配列の要素を出力
-while (i < holidays.length) {
+while (i < 16) {
   console.log(holidays[i]);i++;
 }
