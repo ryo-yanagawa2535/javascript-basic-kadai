ea8df5ebb383e0bdb48309c5bc449df99c37045e
ryo-yanagawa2535 <ryosan1108@gmail.com>
2024-10-20 09:34:38 +0900

    提出1

---
+++ kadai_021/JavaScript.js
@@ -0,0 +1,5 @@
+let name = "Alice";
+let age = 30;
+
+console.log("Name:", name); // 変数の値を表示
+console.log("Age:", age);
--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,8 +1,10 @@
 const Btn = document.getElementById('btn');
 
+const Text = document.getElementById('text');
+
 Btn.addEventListener('click', () => {
   setTimeout(() => {
-    text.textContent = 'ボタンをクリックしました';
+    Text.textContent = 'ボタンをクリックしました';
   }, 2000);
 });
 
